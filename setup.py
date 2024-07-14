fbe5aa1b85e1832aff7daaeeaeab14aa560e9324
MohitKapoor19 <mohit22csu349@ncuindia.edu>
2024-07-14 19:50:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
 REPO_NAME = "coding-raja-technologies-internship"
 AUTHOR_USER_NAME = "MohitKapoor19"
 SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = ['streamlit']
-
+LIST_OF_REQUIREMENTS = [
+    'streamlit',
+    'pandas',
+    'requests',
+    'numpy',
+    'scikit-learn'
+]
 
 setup(
     name=SRC_REPO,
@@ -17,10 +21,14 @@ setup(
     description="A small package for Movie Recommender System",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/MohitKapoor19/take_two",
+    url="https://github.com/MohitKapoor19/coding-raja-technologies-internship",
     author_email="mohit22csu349@ncuindia.edu",
-    packages=[SRC_REPO],
-    license="MIT",
-    python_requires=">=3.12.4",
-    install_requires=LIST_OF_REQUIREMENTS
+    packages=find_packages(where=SRC_REPO),
+    package_dir={"": SRC_REPO},
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
 )
